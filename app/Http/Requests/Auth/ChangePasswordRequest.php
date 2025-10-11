efba8a76cd94a1f4a2a3fdf3b39f98ef73b11faa
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2025-10-11 21:39:48 +0200

    Update app/Http/Requests/Auth/ChangePasswordRequest.php
    Co-authored-by: Dominik Purgał <amonsir29@gmail.com>

--- app/Http/Requests/Auth/ChangePasswordRequest.php
+++ app/Http/Requests/Auth/ChangePasswordRequest.php
@@ -15,8 +15,6 @@ class ChangePasswordRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
