3b865eb314891e27661a3ff9a3201f2554b63cfc
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2025-10-11 21:41:07 +0200

    Update app/Http/Requests/Auth/RegisterRequest.php
    Co-authored-by: Dominik Purgał <amonsir29@gmail.com>

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -9,9 +9,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
