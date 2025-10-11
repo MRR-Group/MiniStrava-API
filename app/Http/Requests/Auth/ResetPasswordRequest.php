9e6d1fa417a06cfbc3ff906b20fb940e692bb71d
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2025-10-11 21:39:23 +0200

    Update app/Http/Requests/Auth/ResetPasswordRequest.php
    Co-authored-by: Dominik Purgał <amonsir29@gmail.com>

--- app/Http/Requests/Auth/ResetPasswordRequest.php
+++ app/Http/Requests/Auth/ResetPasswordRequest.php
@@ -18,8 +18,6 @@ class ResetPasswordRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
