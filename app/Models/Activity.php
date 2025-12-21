4d598b798b7d55fce63fc2bb740ed3b93d7d52f4
Kacper Walenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-12-21 21:03:50 +0100

    update Activity.php

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Strava\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
