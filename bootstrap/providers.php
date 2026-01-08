6f39dd01206fb61833349fa11b260934e17d031b
Kacper Walenga <kacper.walenga@studenci.collegiumwitelona.pl>
2026-01-08 21:53:49 +0100

    init filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-use Strava\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    Strava\Providers\AppServiceProvider::class,
+    Strava\Providers\Filament\AdminPanelProvider::class,
 ];
